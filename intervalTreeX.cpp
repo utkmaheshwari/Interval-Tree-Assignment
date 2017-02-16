f713d0af32201634f40d0ffb19803e87d00301ec
utkmaheshwari <shanu.um123@gmail.com>
2017-02-16 23:52:00 +0530

    slight modification

--- intervalTreeX.cpp
+++ intervalTreeX.cpp
@@ -7,12 +7,12 @@ Class : B.Tech, 4th Year, Electrical Engineering
 #include<iostream>
 using namespace std;
 
-struct time{//structure to store time
+struct range{//structure to store time
 	int x,y;//x->low endpoint and y->high endpoint of the time range
 };
 
 struct node{//structure for each node of the interval tree
-	time t;// time range of the node
+	range r;// time range of the node
 	int high;//stores the maximum endpoint value of the subtree
 	node *left;//pointer to left subtree
 	node *right;//pointer to right subtree
@@ -26,22 +26,22 @@ int getRightMaxi(node *root){//returns the maximum endpoint of the right subtree
 	return (root->right==NULL)?0:root->right->high;
 }
 
-void insert(node **rootptr, time t){//insert time t into the interval tree
+void insert(node **rootptr, range r){//insert time t into the interval tree
 	if((*rootptr)==NULL){//base case
 		(*rootptr)=new node();
-		(*rootptr)->t=t;
-		(*rootptr)->high=t.y;
+		(*rootptr)->r=r;
+		(*rootptr)->high=r.y;
 		(*rootptr)->left=NULL;
 		(*rootptr)->right=NULL;
 	}
-	else if(t.x<(*rootptr)->t.x){//node to be inserted in left subtree
-		insert(&((*rootptr)->left),t);	
+	else if(r.x<(*rootptr)->r.x){//node to be inserted in left subtree
+		insert(&((*rootptr)->left),r);	
 	} 
-	else if(t.x>(*rootptr)->t.x){//node to be inserted in right subtree
-		insert(&((*rootptr)->right),t);	
+	else if(r.x>(*rootptr)->r.x){//node to be inserted in right subtree
+		insert(&((*rootptr)->right),r);	
 	} 
 	//maintain the augumented data oh "high" after insertion
-	(*rootptr)->high=max((*rootptr)->t.y,max(getLeftMaxi(*rootptr),getRightMaxi(*rootptr)));
+	(*rootptr)->high=max((*rootptr)->r.y,max(getLeftMaxi(*rootptr),getRightMaxi(*rootptr)));
 }
 
 void inorder(node *root){//prints the inorder traversal of the interval tree
@@ -49,22 +49,22 @@ void inorder(node *root){//prints the inorder traversal of the interval tree
 		return;	
 	} 
 	inorder(root->left);
-	cout<<root->t.x<<":"<<root->t.y<<":"<<root->high<<" ";
+	cout<<root->r.x<<":"<<root->r.y<<":"<<root->high<<" ";
 	inorder(root->right);
 }
 
-void search(node *root, time t){//finds an overlapping time range from the tree
+void search(node *root, range r){//finds an overlapping time range from the tree
 	if(root==NULL){//No overlapping range case
 		cout<<"No Overlap"<<endl;	
 	}
-	else if((root->t.x<=t.y)&&(t.x<=root->t.y)){//overlapping range found case
-		cout<<root->t.x<<":"<<root->t.y<<endl;	
+	else if((root->r.x<=r.y)&&(r.x<=root->r.y)){//overlapping range found case
+		cout<<root->r.x<<":"<<root->r.y<<endl;	
 	}
-	else if((root->left!=NULL)&&(root->left->high>=t.x)){//overlapping range in left subtree
-		search(root->left,t);
+	else if((root->left!=NULL)&&(root->left->high>=r.x)){//overlapping range in left subtree
+		search(root->left,r);
 	}
 	else{//overlapping range in right subtree
-		search(root->right,t);
+		search(root->right,r);
 	}
 }
 
@@ -78,11 +78,11 @@ node* successor(node *temp){//returns the successor of a node from the tree
 	return leftmost(temp->right);
 }
 
-void del(node **rootptr,time t){//delete a time range from the tree
+void del(node **rootptr,range r){//delete a time range from the tree
 	if((*rootptr)==NULL){
 		cout<<"Not in the tree"<<endl;
 	} 
-	else if(((*rootptr)->t.x==t.x)&&((*rootptr)->t.y==t.y)){
+	else if(((*rootptr)->r.x==r.x)&&((*rootptr)->r.y==r.y)){
 		if(((*rootptr)->left==NULL)&&((*rootptr)->right==NULL)){//leaf node case
 			(*rootptr)=NULL;
 			return;	
@@ -95,20 +95,20 @@ void del(node **rootptr,time t){//delete a time range from the tree
 		} 
 		else{//both left and right subtree case
 			node *temp=successor((*rootptr));
-			(*rootptr)->t.x=temp->t.x;
-			(*rootptr)->t.y=temp->t.y;
+			(*rootptr)->r.x=temp->r.x;
+			(*rootptr)->r.y=temp->r.y;
 			(*rootptr)->high=temp->high;
-			del(&((*rootptr)->right),temp->t);	
+			del(&((*rootptr)->right),temp->r);	
 		}
 	}
-	else if(t.x<(*rootptr)->t.x){//node to be deleted in left subtree
-		del(&((*rootptr)->left),t);
+	else if(r.x<(*rootptr)->r.x){//node to be deleted in left subtree
+		del(&((*rootptr)->left),r);
 	}
-	else if(t.x>(*rootptr)->t.x){//node to be deleted in right subtree
-		del(&((*rootptr)->right),t);	
+	else if(r.x>(*rootptr)->r.x){//node to be deleted in right subtree
+		del(&((*rootptr)->right),r);	
 	}
 	//maintain the augumented data oh "high" after deletion 
-	(*rootptr)->high=max((*rootptr)->t.y,max(getLeftMaxi(*rootptr),getRightMaxi(*rootptr)));
+	(*rootptr)->high=max((*rootptr)->r.y,max(getLeftMaxi(*rootptr),getRightMaxi(*rootptr)));
 }
 
 int main(){
@@ -122,19 +122,19 @@ int main(){
 	while(true){
 		cin>>n;
 		if(n==1){
-			time t;
-			cin>>t.x>>t.y;
-			insert(&root,t);//insertion
+			range r;
+			cin>>r.x>>r.y;
+			insert(&root,r);//insertion
 		}
 		else if(n==2){
-			time t;
-			cin>>t.x>>t.y;
-			del(&root,t);//deletion
+			range r;
+			cin>>r.x>>r.y;
+			del(&root,r);//deletion
 		}
 		else if(n==3){
-			time t;
-			cin>>t.x>>t.y;
-			search(root,t);//searching overlapping time range
+			range r;
+			cin>>r.x>>r.y;
+			search(root,r);//searching overlapping time range
 		}	
 		else if(n==4){
 			inorder(root);//inorder traversal
